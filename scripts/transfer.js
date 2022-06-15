d2ee30b580434d4e82b6ab32b5c23df51839e0c7
zocodo <1175353458@qq.com>
2022-06-15 10:45:47 +0800

    翻译为圈x语法

---
+++ scripts/transfer.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const axios = require('axios')
+axios.get('https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts').then((res) => {
+  const { data } = res;
+  const newdata = data.replace(/127\.0\.0\.1|localhost|::1/g, '')
+  const hostarr = newdata.split(/[\r\n]+/);
+  let text = ''
+  hostarr.forEach((item) => {
+    item = item.replace(/\s|\#.+/g, '')
+    if (item) {
+      text += 'host-keyword, ' + item + ', AdBlock\n';
+    }
+  })
+  fs.writeFile('./adwars-quantumultX.txt', text, function() {
+    console.log('ok')
+  });
+})
+
