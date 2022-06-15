94f32d71f857a0d5f2a39991f24d6761f4bbb9c8
zocodo <1175353458@qq.com>
2022-06-15 11:32:18 +0800

    feat: 自动推代码

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -4,7 +4,7 @@ axios.get('https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts').the
   const { data } = res;
   const newdata = data.replace(/127\.0\.0\.1|localhost|::1/g, '')
   const hostarr = newdata.split(/[\r\n]+/);
-  let text = ''
+  let text = '# modification time: ' + Date.now() + '\n';
   hostarr.forEach((item) => {
     item = item.replace(/\s|\#.+/g, '')
     if (item) {
