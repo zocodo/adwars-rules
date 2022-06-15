5baff8c3d64236105150b173ddaf3aa19cf11951
zocodo <1175353458@qq.com>
2022-06-15 11:38:48 +0800

    feat: 自动推代码

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -1,10 +1,14 @@
 const fs = require('fs')
 const axios = require('axios')
-axios.get('https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts').then((res) => {
+const url = 'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts'
+axios.get(url).then((res) => {
   const { data } = res;
   const newdata = data.replace(/127\.0\.0\.1|localhost|::1/g, '')
   const hostarr = newdata.split(/[\r\n]+/);
-  let text = '# modification time: ' + Date.now() + '\n';
+  const now = new Date()
+  let text = `# by: zocodo \n`;
+  text += `# modification time: ${now.toLocaleDateString()}${now.toLocaleTimeString()}\n`;
+  text += `# from: ${url}\n`;
   hostarr.forEach((item) => {
     item = item.replace(/\s|\#.+/g, '')
     if (item) {
